ddbfd4b905275d2b8e73d6c262c4f944b407e4c8
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-02-07 15:14:17 +0530

    LCM Solution Add

---
+++ Coding_Blocks_Java_DSA_Live_Jan_8_24/src/Assignment_1_Pattern/P_5_LCM.java
@@ -0,0 +1,25 @@
+package Assignment_1_Pattern;
+
+import java.util.Scanner;
+
+public class P_5_LCM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int lcm = 0;
+
+		int max = (num1 > num2) ? num1 : num2;
+		for (int i = max; i <= num1 * num2; i++) {
+			if (i % num1 == 0 && i % num2 == 0) {
+				lcm = i;
+				break;
+			}
+		}
+		System.out.println(lcm);
+
+	}
+
+}
