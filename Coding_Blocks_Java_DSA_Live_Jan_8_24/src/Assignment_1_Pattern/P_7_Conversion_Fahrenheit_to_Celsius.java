e136d2f58941fbb7a052effe010f46e5f77aab05
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-02-07 15:27:17 +0530

    Conversion Fahrenheit to Celcius Solution Add

---
+++ Coding_Blocks_Java_DSA_Live_Jan_8_24/src/Assignment_1_Pattern/P_7_Conversion_Fahrenheit_to_Celsius.java
@@ -0,0 +1,21 @@
+package Assignment_1_Pattern;
+
+import java.util.Scanner;
+
+public class P_7_Conversion_Fahrenheit_to_Celsius {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int min = sc.nextInt();
+		int max = sc.nextInt();
+		int step = sc.nextInt();
+		
+		for(int fahrenheit = min; fahrenheit<=max; fahrenheit += step) {
+			int celsius = ((fahrenheit-32)*5)/9;
+			System.out.println(fahrenheit + "\t" +celsius);
+		}
+
+	}
+
+}
