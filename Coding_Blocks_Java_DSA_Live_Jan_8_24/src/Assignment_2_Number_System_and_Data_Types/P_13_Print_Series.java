cb819c4154860a03dffb1563e6d50ba80199d365
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-02-07 16:02:32 +0530

    Print Series

---
+++ Coding_Blocks_Java_DSA_Live_Jan_8_24/src/Assignment_2_Number_System_and_Data_Types/P_13_Print_Series.java
@@ -0,0 +1,25 @@
+package Assignment_2_Number_System_and_Data_Types;
+
+import java.util.Scanner;
+
+public class P_13_Print_Series {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int count = 0;
+		int i = 1;
+		while(count<=n1) {
+			int term = 3*i+2;
+			i++;
+			if(term % n2 != 0) {
+				System.out.println(term);		
+			}
+			count++;
+		}
+
+	}
+
+}
