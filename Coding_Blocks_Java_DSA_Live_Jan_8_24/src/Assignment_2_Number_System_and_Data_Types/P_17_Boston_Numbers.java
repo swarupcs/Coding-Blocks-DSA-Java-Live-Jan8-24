eac95d114c32d0689c57dacfabddff0d62d8d6f9
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-02-11 21:55:33 +0530

    Boston Number Solution Add

--- Coding_Blocks_Java_DSA_Live_Jan_8_24/src/Assignment_2_Number_System_and_Data_Types/P_17_Boston_Numbers.java
+++ Coding_Blocks_Java_DSA_Live_Jan_8_24/src/Assignment_2_Number_System_and_Data_Types/P_17_Boston_Numbers.java
@@ -8,11 +8,29 @@ public class P_17_Boston_Numbers {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		System.out.println(Is_Boston_Numbers(n));
 
 	}
 	
 	public static int Is_Boston_Numbers(int n) {
 		int digit = sumOfdigit(n);
+		int primeFactor = 0;
+		int div = 2;
+		while(div <= n) {
+			int rem = n%div;
+			if(rem == 0) {
+				primeFactor = primeFactor + sumOfdigit(div);
+				n = n / div;				
+			} else {
+				div++;
+			}
+		}
+		
+		if(primeFactor == digit) {
+			return 1;
+		} else {
+			return 0;
+		}
 	}
 	
 	public static int sumOfdigit(int num) {
