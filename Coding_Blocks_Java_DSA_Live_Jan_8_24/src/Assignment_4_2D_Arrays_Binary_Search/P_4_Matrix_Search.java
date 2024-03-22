94d289ebd80440da8f022316609dfe0408a871ff
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-03-22 17:57:03 +0530

    Transpose of a square matrix

--- Coding_Blocks_Java_DSA_Live_Jan_8_24/src/Assignment_4_2D_Arrays_Binary_Search/P_4_Matrix_Search.java
+++ Coding_Blocks_Java_DSA_Live_Jan_8_24/src/Assignment_4_2D_Arrays_Binary_Search/P_4_Matrix_Search.java
@@ -1,10 +1,40 @@
 package Assignment_4_2D_Arrays_Binary_Search;
 
+import java.util.Scanner;
+
 public class P_4_Matrix_Search {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int m = scn.nextInt();
+	       int n = scn.nextInt();
+	       int[][] arr=new int[m][n];
+	       for (int i=0; i<m; i++){
+	              for(int j=0; j<n; j++){
+	                    arr[i][j]=scn.nextInt();
+	               }
+	        }
+	        int k = scn.nextInt();
+	        System.out.print(searchMatrix(arr, k));
 
 	}
+	public static int searchMatrix(int[][] matrix, int target) {
+    	int row = 0;
+    	int col = matrix[0].length-1;
+    	
+    	while(row<matrix.length && col >=0) {
+    			if(matrix[row][col] == target) {
+    				return 1;
+    			} else if (matrix[row][col] > target) {
+					col--;
+				} else {
+					row++;
+				}
+    		}
+
+		return 0;
+
+    }
 
 }
---
+++ Coding_Blocks_Java_DSA_Live_Jan_8_24/src/Assignment_4_2D_Arrays_Binary_Search/P_8_Transpose_of_a_square_matrix.java
@@ -0,0 +1,40 @@
+package Assignment_4_2D_Arrays_Binary_Search;
+
+import java.util.Scanner;
+
+public class P_8_Transpose_of_a_square_matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		TransposeMatrix(arr);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static void TransposeMatrix(int[][] arr) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<arr.length; i++) {
+			for(int j=i+1; j<arr[0].length; j++) {
+				int temp = arr[i][j];
+				arr[i][j] = arr[j][i];
+				arr[j][i] = temp;
+			}
+		}
+		
+	}
+
+}
